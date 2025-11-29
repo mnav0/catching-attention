136023e82b57a37b6c0208fd04ba22771a501ac5
Maggie Navracruz <mnavracruz@gmail.com>
2025-11-29 12:12:40 -0500

    add base path to config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import dsv from '@rollup/plugin-dsv';
 
 export default defineConfig({
+  base: '/catching-attention/', // base public path when served in production
   plugins: [
     dsv(), // Add the dsv plugin
   ],
